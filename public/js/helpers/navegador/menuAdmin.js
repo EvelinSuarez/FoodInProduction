3cba9fa30ff5d66d1a5be5bf8eb9e23f3fe9c88c
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2024-03-09 10:29:10 -0500

    cambios generales

--- public/js/helpers/navegador/menuAdmin.js
+++ public/js/helpers/navegador/menuAdmin.js
@@ -29,6 +29,6 @@ optionConfiguracion.addEventListener("change", (e) => {
   } else if (stock === "PerfilSecundario") {
     location.href = "../../modulos/config/secundario.html";
   } else if (stock === "CerrarSesion") {
-    location.href = "../../index.html";
+    location.href = "../../modulos/index.html";
   }
 });
